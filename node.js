370214875efb4e31946f5764f4747b3a5be3cda4
Abdulhamid-Abdurasulov <abdurasulovabdulhamid701@gmail.com>
2023-03-28 16:09:26 +0500

    gf

---
+++ node.js
@@ -0,0 +1,9 @@
+
+
+function girls(du ,su ,mu ,ru){
+    if( 2000000 >= du + mu + su + ru){
+        return "pulingiz yetadi"
+    }else{
+        return "pulingiz yetmaydi"
+    }}
+console.log(girls(50000, 50000, 50000, 50000));
